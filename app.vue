dcfcc8b8ac7faf6d21c0ff69cf302e4c3c83758d
Orolol <gangngank@gmail.com>
2022-11-22 00:25:38 +0100

    First  commit

---
+++ app.vue
@@ -0,0 +1,4 @@
+<template lang="pug">
+.main-container  
+  router-view
+</template>
---
+++ components/login.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+.login-container   
+    form(@submit='getLogin')
+        v-text-field.login-input(label="Login" hide-details="auto" v-model='login')
+        v-text-field.login-input(label="Password" hide-details="auto" v-model='password')
+        v-btn.login-input(type='submit') Auth
+
+</template>
+
+<script lang="ts" setup>
+
+let login = ref('');
+let password = ref('');
+let errorMessageCmp = ref('');
+
+const getLogin = (e : SubmitEvent) => {
+    e.preventDefault();
+    
+    console.log(login.value, password.value)
+}
+
+</script>
